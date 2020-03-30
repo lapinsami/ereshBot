9a892839c36dfb710a8a80cfc40836df9f021b22
Vogelchevalier <lappalainen.sami@outlook.com>
2020-03-30 21:00:18 +0300

    Dab

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import File
 
 
 class Misc(commands.Cog):
@@ -7,11 +8,17 @@ class Misc(commands.Cog):
         self.bot = bot
 
     @commands.command(name='rin',
-                      description="The truth about Rin",
-                      brief="- The truth",
+                      description='The truth about Rin',
+                      brief='The truth',
                       aliases=['eresh'])
     async def rin(self, ctx):
         await ctx.send("Ereshkigal > Ishtar > f/sn Rin > Loli Rin > f/ha Rin")
+
+    @commands.command(name='dab',
+                      description='Ereshkigal dabs',
+                      brief='Eresh dabs')
+    async def dab(self, ctx):
+        await ctx.send(file=File('eresh-dab.png'))
 
 
 def setup(bot):
