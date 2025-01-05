a4c6288320ec499da6237473deacb0ee1531a699
Sami Lappalainen <lappalainen.sami@outlook.com>
2025-01-05 15:46:04 +0100

    removed all commands and reimplemented --ping

--- application/cogs/dev.py
+++ application/cogs/dev.py
@@ -1,11 +1,15 @@
 from discord.ext import commands
 
-
 class Dev(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="ping",
+                      description="pong")
+    async def ping(self, ctx):
+        await ctx.send("pong")
 
-def setup(bot):
-    bot.add_cog(Dev(bot))
+
+async def setup(bot):
+    await bot.add_cog(Dev(bot))
--- ereshBot.py
+++ ereshBot.py
@@ -1,46 +1,25 @@
+import os
+import asyncio
 import discord
 from discord.ext import commands
-from application import BotState, Auth
 from discord.ext.commands import CommandNotFound
+from application import COGS
 
+TOKEN = os.environ['DISCORD_TOKEN']
 
-def getPrefix(botti, message):
-    prefixes = ['--', 'eresh ']
-    return commands.when_mentioned_or(*prefixes)(botti, message)
-
-
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.members = True
-
-bot = commands.Bot(command_prefix=getPrefix, intents=intents)
+bot = commands.Bot(command_prefix="--", intents=intents)
 
 
 @bot.check
-async def commandAllowed(ctx):
-
-    # Check if user banned
-    if ctx.message.author.id in BotState.PERMS.keys():
-        if BotState.PERMS[ctx.message.author.id]["banned"]:
-            print("banned")
-            return False
-
-    # Check if inside a PM
-    # and if the user has permission to use commands in PMs
-    # or if command is allowed in PMs for everyone
-    if isinstance(ctx.message.channel, discord.DMChannel):
-        if ctx.message.author.id == 76579685995118592:
-            return True
-        elif ctx.message.author.id in BotState.PERMS.keys():
-            return BotState.PERMS[ctx.message.author.id]["pm_user"] or BotState.PERMS[ctx.message.author.id]["admin"]
-        else:
-            return False
-    else:
-        return True
+async def command_allowed(ctx):
+    return True
 
 
 @bot.event
 async def on_ready():
-    print(f'\n# Logged in as {bot.user.name} ({BotState.STATUS["nickname"]}), id {bot.user.id}')
+    print(f'\n# Logged in as {bot.user.name}, id {bot.user.id}')
 
 
 @bot.event
@@ -49,10 +28,14 @@ async def on_command_error(ctx, error):
         return
     raise error
 
+async def load_extensions():
+    for ext in COGS:
+        await bot.load_extension(f'application.{ext}')
+        print(f"# {ext} loaded")
 
-for extension in BotState.STATUS["availableCogs"]:
-    if extension not in BotState.STATUS["disabledCogs"]:
-        bot.load_extension(f'application.{extension}')
-        print(f"# {extension} loaded")
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(TOKEN, reconnect=True)
 
-bot.run(Auth.DISCORD, bot=True, reconnect=True)
+asyncio.run(main())
